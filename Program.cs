3998c6eaa4235c126e471fcfef3be01417ff475d
Gary <66178774+gen8787@users.noreply.github.com>
2021-01-21 22:35:22 -0800

    Assignment One

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Printing numbers 50 down to 35, EXCEPT 43 and 37.");
+
+            int ReturnFifty = Return50();
+            int ReturnThirtyFive = Return35();
+            
+            Program p = new Program();
+            p.Print50to35(ReturnFifty, ReturnThirtyFive);
+        }
+
+        public static int Return50()
+        {
+            return 50;
+        }
+
+        public static int Return35()
+        {
+            return 35;
+        }
+
+        public void Print50to35(int start, int end)
+        {
+            for (int i = start; i >= end; i --)
+            {
+                if (i != 43 && i != 37)
+                {
+                    System.Console.WriteLine(i);
+                }
+            }
+        }
+    // End of Program class
+    }
+}
---
+++ README.md
@@ -0,0 +1,26 @@
+### Please deliver an DotNet Core project with:
+
+* A console base application
+
+* Has some methods that call each other (demonstrate that you understand how to declare methods, call them, pass data and return data from a method).
+
+* Print the numbers from 50 to 35, except 37 and 43.
+
+###### Terminal Output:
+```
+Printing numbers 50 down to 35, EXCEPT 43 and 37.
+50
+49
+48
+47
+46
+45
+44
+42
+41
+40
+39
+38
+36
+35
+```
